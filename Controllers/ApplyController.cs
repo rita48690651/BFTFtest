7727b0ac0d6038e3aad658b8580ba4bb5961d308
rita48690651@gmail.com <rita48690651@gmail.com>
2021-08-31 15:05:44 +0800

    加入專案檔案。

---
+++ Controllers/ApplyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BFTFtest.Models;
+
+namespace BFTFtest.Controllers
+{
+    public class ApplyController : Controller
+    {
+        // GET: Apply
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+            {
+                List<Apply> empList = db.Apply.ToList<Apply>();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BFTFtest.MvcApplication" Language="C#" %>
